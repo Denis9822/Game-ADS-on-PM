70a434539d60106282826860d5a0b3a5a6a3bbb3
Denis <ss.set2013@yandex.ru>
2017-11-23 12:03:31 +0200

    NEW

---
+++ Assets/LoadAds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class LoadAds : MonoBehaviour {
+	
+	public void ShowVideo()
+	{
+
+		if (Advertisement.isSupported) {
+			Advertisement.Initialize ("1586786",false);
+
+
+		} else {
+			Debug.Log ("НЕТ РЕКЛАПМЫ");
+		}
+
+
+
+
+		StartCoroutine ("ShowADS");
+
+
+
+
+
+	}
+
+
+
+	IEnumerator ShowADS(){
+
+		yield return new WaitForSeconds (0.5f);
+		while (!Advertisement.IsReady ())
+			yield return null;
+		Advertisement.Show ();
+
+	}
+
+}
+
